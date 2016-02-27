a58da6149ac9c2e2535f71f9d53ccc43296232b8
anizado <ancizar.valencia@gmail.com>
2016-02-27 10:07:32 -0500

    primer file del repositorio

---
+++ Tutorial.py
@@ -0,0 +1,36 @@
+
+# coding: utf-8
+
+# ### Puedo hacer una rayita
+# <hr style="height:4px">
+
+# # Puedo tener un encabezado de primer nivel
+# ## O uno de segundo nivel
+# ### Incluso uno de tercero
+# 
+# Puedo escribir normalito.
+# > Y tambien puedo identar
+
+# <h1><center>**Tutorial Markdown R **</center><h1>
+# <hr style="height:3px">
+
+# <p>Segun dice en Wikipedia, Markdown es un lenguaje de marcado ligero creado por John Gruber que trata de conseguir la máxima legibilidad y facilidad de publicación tanto en sus forma de entrada como de salida, inspirándose en muchas convenciones existentes para marcar mensajes de correo electrónico usando texto plano. Se distribuye bajo licencia BSD y se distribuye como plugin (o al menos está disponible) en diferentes sistemas de gestión de contenidos (CMS). Markdown convierte el texto marcado en documentos XHTML utlizando html2text creado por Aaron Swartz [2]. Markdown fue implementado originariamente en Perl por Gruber, pero desde entonces ha sido traducido a multitud de lenguajes de programación, incluyendo PHP, Python, Ruby, Java y Common Lisp.</p>
+
+# <h2><center>**Párametros por defecto **</center><h2>
+
+# <p><strong>Title: </strong>Título del archivo</p>
+# <p><strong>Author: </strong>Nombre del autor</p>
+# <p><strong>Date: </strong>Fecha de creación del archivo</p>
+# <p><strong>output: </strong>Tipo de documento de salida</p>
+
+# <h2><center>**Notaciones **</center><h2>
+
+# <p><strong>\* texto \*: </strong>Italica</p>
+# <p><strong>\*\* texto \*\*: </strong>Negrita</p>
+# <p><strong>guion: </strong>Usar guion para una lista</p>  
+# <p><strong>Tíldes al revés(`): </strong>Insertar codigo en el archivo original</p> 
+
+# In[ ]:
+
+
+
